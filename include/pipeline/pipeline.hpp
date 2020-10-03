5aa248a047cfd6402ea43f93bef2d1f0f988e30f
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2020-10-03 18:59:25 -0500

    Added convenience header file

--- include/pipeline/pipeline.hpp
+++ include/pipeline/pipeline.hpp
@@ -1,2 +1,7 @@
 #pragma once
-#include <pipeline.hpp/details.hpp>
+#include <pipeline/fn.hpp>
+#include <pipeline/pipe.hpp>
+#include <pipeline/fork.hpp>
+#include <pipeline/fork_async.hpp>
+#include <pipeline/unzip_into.hpp>
+#include <pipeline/unzip_into_async.hpp>
